c9b125baeb9cf48ee1bd5e02ccfd99c9e36055a7
Elizabeth Flores <santiagosil3434@gmail.com>
2024-01-08 11:08:38 -0300

    better start.sh

--- start.sh
+++ start.sh
@@ -1,22 +1,28 @@
 #!/usr/bin/env zsh
 
-DOTFILES=$HOME/.dotfiles
-
-if [[ -z $STOW_FOLDERS ]]; then
-	STOW_FOLDERS="bin,personal,nvim,tmux,zsh"
-fi
-
-#brew install git
-brew install stow
-
 # [[ TMUX PLUGIN MANAGER ]]
 # git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 # [[ OH MY ZSH ]]
 # sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
+DOTFILES=$HOME/.dotfiles
+if [[ -z $STOW_FOLDERS ]]; then
+	STOW_FOLDERS="bin,personal,nvim,tmux,zsh"
+fi
 
+# [[ BREW ]]
+command -v brew >/dev/null 2>&1 || { echo >&2 "Installing Homebrew"; \
+git clone https://github.com/Homebrew/brew homebrew }
+eval "$(homebrew/bin/brew shellenv)"
+brew update --force --quiet
+chmod -R go-w "$(brew --prefix)/share/zsh"
+brew install stow
+
+
+# [[ STOW ]]
 pushd $DOTFILES
 git clone https://github.com/Santiago-Silvera/nvim ~/.dotfiles/nvim/.config/nvim
+rm ~/.zprofile
 for folder in $(echo $STOW_FOLDERS | sed "s/,/ /g")
 do
 	# echo "stow $folder"
